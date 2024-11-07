58deea54c12e6c89b6709ab6ee3f7d321b39d4e3
AdrewReyes <andrewreyes.m@gmail.com>
2024-11-07 05:02:37 +0000

    JS conditionals

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 // all code for this lab to be completed in this file
+let currentTemerature = 70; 
+
+
+if  (currentTemerature > 32) {
+    console.log ("is it not Freezing")
+}   else {
+    console.log ("it is freezing")
+}
+currentTemerature = 20;
+
+if  (currentTemerature > 32) {
+    console.log ("is it not Freezing")
+}   else {
+    console.log ("it is freezing")
+}
+
+
+let age1 = 33
+let age2 = 32
+
+//older
+if (age1 > age2) { 
+    console.log("Hey I'm older than you!")
+} else if (age1 < age2) {
+    //younger
+    console.log("Man! You're old!")
+} else if (age1 === age2) {
+    //equal age
+    console.log("Sweet! We're the same age")
+} else {
+    console.log("Dude you put in the wrong info. Try again!")
+}
+
+//isLoggedin Conditional Challenge
+let isLoggedIn =  false
+
+if (isLoggedIn) {
+    console.log ("Thank you For logging in Today")
+} else {
+    console.log ("Please log in")
+}
